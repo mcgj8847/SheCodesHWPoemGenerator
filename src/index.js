ec08b2e8998404f1c7185f5270a5849286dc2cfc
mcgj8847 <mcguzmanj@uoc.edu>
2025-06-15 20:45:05 -0600

    Updated Html, Css and Js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings:
+      "O, never say that I was false of heart,Though absence seemed my flame to qualify.",
+    autoStart: true,
+    cursor: "",
+  });
+}
 
+let poemForm = document.querySelector("#form");
+poemForm.addEventListener("submit", generatePoem);
